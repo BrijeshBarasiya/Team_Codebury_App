f4e95e7dddb185d169e0efc0c08bb5747b97d655
Shubham Vyas <svyas878@gmail.com>
2022-04-02 12:23:54 +0530

    Animated Splash Screen Added

---
+++ app/src/main/java/com/codebury/simfocus/splash_screen/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.codebury.simfocus.splash_screen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import com.codebury.simfocus.MainActivity
+import com.codebury.simfocus.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var topAnimation: Animation
+    private lateinit var bottomAnimation: Animation
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_up_to_bottom_splash)
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_bottom_to_up_splash)
+
+        val topPartLogo: ImageView = findViewById(R.id.logoSplash)
+        val bottomPartTv: TextView = findViewById(R.id.motoSplash)
+
+        topPartLogo.animation = topAnimation
+        bottomPartTv.animation = bottomAnimation
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+
+    }
+}
